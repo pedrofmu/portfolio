8912ec45f518302fb5b0c3de2682a5b10a1b201a
pedrofm3000 <pedrofm3000@gmail.com>
2023-10-19 13:58:10 +0200

    First commit

---
+++ code/animation.js
@@ -0,0 +1,50 @@
+const canvas = document.getElementById('background_effect');
+const context = canvas.getContext('2d');
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+// Llama a resizeCanvas al cargar la página y al redimensionar la ventana
+window.addEventListener('load', resizeCanvas);
+window.addEventListener('resize', resizeCanvas);
+
+// Llama a draw después de cambiar el tamaño del canvas
+resizeCanvas();
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
+const alphabet = katakana + latin + nums;
+
+const fontSize = 16;
+const columns = canvas.width/fontSize;
+
+const rainDrops = [];
+
+for( let x = 0; x < columns; x++ ) {
+	rainDrops[x] = 1;
+}
+
+const draw = () => {
+	context.fillStyle = 'rgba(12, 30, 39, 0.5)';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	context.fillStyle = '#94512e';
+	context.font = fontSize + 'px monospace';
+
+	for(let i = 0; i < rainDrops.length; i++)
+	{
+		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+		
+		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+			rainDrops[i] = 0;
+        }
+		rainDrops[i]++;
+	}
+};
+
+setInterval(draw, 30);
---
+++ code/buttonsActions.js
@@ -0,0 +1,32 @@
+document.getElementById("about_btn").addEventListener("click", function (){
+    var destino = document.getElementById("about_part");
+
+     // Calcula la posición de desplazamiento en vh
+     var vhValue = 10; // Cambia este valor según tus preferencias
+
+     // Convierte vh a píxeles (1 vh es el 1% de la altura de la ventana)
+     var offset = (vhValue * window.innerHeight) / 100;
+
+     // Realiza el desplazamiento suave hacia la posición calculada
+     window.scrollTo({ top: destino.offsetTop - offset, behavior: "smooth" });
+}); 
+
+document.getElementById("proyectos_btn").addEventListener("click", function (){
+    var destino = document.getElementById("proyectos_part");
+
+     // Calcula la posición de desplazamiento en vh
+     var vhValue = 8; // Cambia este valor según tus preferencias
+
+     // Convierte vh a píxeles (1 vh es el 1% de la altura de la ventana)
+     var offset = (vhValue * window.innerHeight) / 100;
+
+     // Realiza el desplazamiento suave hacia la posición calculada
+     window.scrollTo({ top: destino.offsetTop - offset, behavior: "smooth" });
+}); 
+
+document.getElementById("contact_btn").addEventListener("click", function (){
+    var destino = document.getElementById("contact_part");
+
+     // Realiza el desplazamiento suave hacia la posición calculada
+     destino.scrollIntoView({  behavior: "smooth" });
+}); 
