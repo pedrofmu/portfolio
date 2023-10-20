e84d36aef352b29c51a9246a0eec78df1c191f30
pedrofm3000 <pedrofm3000@gmail.com>
2023-10-20 17:33:58 +0200

    Creado los enclaces al mail y github ademas de mejoras de animaciones

---
+++ code/goToProyect.js
@@ -0,0 +1,11 @@
+document.getElementById("proyect_1").addEventListener("click", function(){
+    window.open("http://www.w3schools.com");
+});
+
+document.getElementById("proyect_2").addEventListener("click", function(){
+    window.open("http://www.w3schools.com");
+});
+
+document.getElementById("proyect_3").addEventListener("click", function(){
+    window.open("http://www.w3schools.com");
+});
