acffa17a5d5d5e612045a775e8a7ddd7062ef5a9
Pedro Fernandez Munoz <pedrofm@pedrofm.dev>
2024-09-22 18:00:02 +0200

    cambiar urls

--- code/goToProyect.js
+++ code/goToProyect.js
@@ -1,5 +1,5 @@
 document.getElementById("proyect_1").addEventListener("click", function(){
-    window.open("https://github.com/pedrofmu/FacturacionFacil");
+    window.open("https://github.com/pedrofmu/facturacion_facil");
 });
 
 document.getElementById("proyect_2").addEventListener("click", function(){
