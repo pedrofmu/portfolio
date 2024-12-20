3e0fa81290bbe63dc4bc16def6f302b89e95b664
Pedro Fernandez Munoz <pedrofm@pedrofm.dev>
2024-12-20 15:46:02 +0100

    add one title

--- code/animation.js
+++ code/animation.js
@@ -20,31 +20,30 @@ const nums = '0123456789';
 const alphabet = katakana + latin + nums;
 
 const fontSize = 16;
-const columns = canvas.width/fontSize;
+const columns = canvas.width / fontSize;
 
 const rainDrops = [];
 
-for( let x = 0; x < columns; x++ ) {
-	rainDrops[x] = 1;
+for (let x = 0; x < columns; x++) {
+    rainDrops[x] = 1;
 }
 
 const draw = () => {
-	context.fillStyle = 'rgba(12, 30, 39, 0.5)';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	
-	context.fillStyle = '#94512e';
-	context.font = fontSize + 'px monospace';
+    context.fillStyle = 'rgba(12, 30, 39, 0.5)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-	for(let i = 0; i < rainDrops.length; i++)
-	{
-		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
-		
-		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
-			rainDrops[i] = 0;
+    context.fillStyle = '#94512e';
+    context.font = fontSize + 'px monospace';
+
+    for (let i = 0; i < rainDrops.length; i++) {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+            rainDrops[i] = 0;
         }
-		rainDrops[i]++;
-	}
+        rainDrops[i]++;
+    }
 };
 
 setInterval(draw, 30);
