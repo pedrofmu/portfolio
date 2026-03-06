75bb27b99bbd14730116eb63cdc7483d64700afe
pedrofm <pedrofm@pedrofm.dev>
2026-03-06 18:41:11 +0100

    Fix build issue

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 const siteUrl = "https://pedrofm.dev";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
