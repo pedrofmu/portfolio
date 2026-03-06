42c4c2a4b1af0dab1c73281ad272b1ba5157a76b
pedrofm <pedrofm@pedrofm.dev>
2026-03-06 18:39:17 +0100

    Fix build issue

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 const siteUrl = "https://pedrofm.dev";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
